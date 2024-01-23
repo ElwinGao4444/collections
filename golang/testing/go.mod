56f45f3dda7dfb5e70c3e28a3d007dad836af4d6
ElwinGao4444 <elwin.gao4444@hotmail.com>
2024-01-23 14:08:27 +0800

    fast update

---
+++ golang/testing/go.mod
@@ -0,0 +1,14 @@
+module main
+
+go 1.21
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/gopherjs/gopherjs v1.17.2 // indirect
+	github.com/jtolds/gls v4.20.0+incompatible // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/smarty/assertions v1.15.0 // indirect
+	github.com/smartystreets/goconvey v1.8.1 // indirect
+	github.com/stretchr/testify v1.8.4 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ golang/testing/main_test.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/smartystreets/goconvey/convey"
+	"github.com/stretchr/testify/assert"
+)
+
+// go test -v -run TestHelloWorld main_test.go
+func TestHelloWorld(t *testing.T) {
+	t.Error("not implemented")
+	t.Fatal("not implemented")
+}
+
+// go test -v -run TestAssert main_test.go
+func TestAssert(t *testing.T) {
+	assert.Equal(t, 123, 123, "they should be equal")
+}
+
+// go test -v -run TestConvey main_test.go
+func TestConvey(t *testing.T) {
+	convey.Convey("TestIsAdxReqeust", t, func() {
+		convey.So(123, convey.ShouldEqual, 123)
+	})
+}
