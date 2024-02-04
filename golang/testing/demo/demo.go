2fe5fb35cb69e50b79d2dbce5ed93e95391f0fe3
ElwinGao4444 <elwin.gao4444@hotmail.com>
2024-02-04 21:41:42 +0800

    fast update

--- golang/testing/demo/demo.go
+++ golang/testing/demo/demo.go
@@ -2,7 +2,8 @@ package demo
 
 // 接口定义
 type Inter interface {
-	Foo(int) error
+	Foo(int) int
+	Bar(int) int
 }
 
 // 驱动类
@@ -11,8 +12,8 @@ type User struct {
 }
 
 // 接口调用
-func (user *User) Use() error {
-	return user.Inter.Foo(0)
+func (user *User) Use() int {
+	return user.Inter.Foo(0) + user.Inter.Bar(0)
 }
 
 // 多接口定义，用于说明GoMock的两种使用模式
--- golang/testing/demo/mock_test.go
+++ golang/testing/demo/mock_test.go
@@ -20,19 +20,57 @@ import (
 // 模式1：源文件模式，Mock指定文件中所有的接口
 // mockgen -source=./demo.go -destination=./demo_mock.go -package=demo
 // 模式2：接口模式，Mock指定路径下指定的接口
-// mockgen -package=demo . Inter > ./demo_mock.go
+// mockgen -package=demo . Inter > ./demo.mock && mv ./demo.mock ./demo_mock.go
 func TestGoMockSourceMode(t *testing.T) {
+	// mock控制器初始化
 	mockCtrl := gomock.NewController(t)
 	defer mockCtrl.Finish()
 
-	// 先设置期望的返回结果
-	mockInter := NewMockInter(mockCtrl)
-	mockInter.EXPECT().Foo(0).Return(nil).Times(1)
+	// 简单用法
+	mockInter1 := NewMockInter(mockCtrl)
+	mockInter1.EXPECT().Foo(0).Return(1).Times(1)
+	mockInter1.EXPECT().Bar(0).Return(2).Times(1)
+	testUser1 := &User{Inter: mockInter1}
+	if n := testUser1.Use(); n != 3 {
+		t.Errorf("result = %v", n)
+	}
 
-	// 再调用方法
-	testUser := &User{Inter: mockInter}
-	err := testUser.Use()
-	if err != nil {
-		t.Errorf("result = %v", err)
+	// 通配用法
+	mockInter2 := NewMockInter(mockCtrl)
+	mockInter2.EXPECT().Foo(gomock.Any()).Return(1).AnyTimes()
+	mockInter2.EXPECT().Bar(gomock.Any()).Return(2).AnyTimes()
+	testUser2 := &User{Inter: mockInter2}
+	if n := testUser2.Use(); n != 3 {
+		t.Errorf("result = %v", n)
+	}
+
+	// 顺序用法(After)
+	mockInter3 := NewMockInter(mockCtrl)
+	var c1 = mockInter3.EXPECT().Foo(gomock.Any()).Return(1).Times(1)
+	var c2 = mockInter3.EXPECT().Bar(gomock.Any()).Return(2).Times(1)
+	mockInter3.EXPECT().Foo(gomock.Any()).Return(3).Times(1).After(c1)
+	mockInter3.EXPECT().Bar(gomock.Any()).Return(4).Times(1).After(c2)
+	testUser3 := &User{Inter: mockInter3}
+	if n := testUser3.Use(); n != 3 {
+		t.Errorf("result = %v", n)
+	}
+	if n := testUser3.Use(); n != 7 {
+		t.Errorf("result = %v", n)
+	}
+
+	// 顺序用法(InOrder)
+	mockInter4 := NewMockInter(mockCtrl)
+	gomock.InOrder(
+		mockInter4.EXPECT().Foo(gomock.Any()).Return(1).Times(1),
+		mockInter4.EXPECT().Bar(gomock.Any()).Return(2).Times(1),
+		mockInter4.EXPECT().Foo(gomock.Any()).Return(3).Times(1),
+		mockInter4.EXPECT().Bar(gomock.Any()).Return(4).Times(1),
+	)
+	testUser4 := &User{Inter: mockInter4}
+	if n := testUser4.Use(); n != 3 {
+		t.Errorf("result = %v", n)
+	}
+	if n := testUser4.Use(); n != 7 {
+		t.Errorf("result = %v", n)
 	}
 }
