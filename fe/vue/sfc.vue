0a82206bd9257f3a6abde8594ec572e0f981303b
ElwinGao4444 <48503777+ElwinGao4444@users.noreply.github.com>
2024-04-05 20:12:05 +0800

    update

--- fe/vue/sfc.vue
+++ fe/vue/sfc.vue
@@ -1,7 +1,9 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, reactive, computed, watch } from 'vue'
 const msg = 'Hello World!'              // 普通变量声明
-const refmsg = ref('Hello World Ref!')  // 响应式变量声明
+const ref_msg = ref('Hello World Ref!')  // 响应式变量声明
+const ref_obj = ref({name:"ref_obj_name", tag:"ref_obj_tag"})
+const reactive_obj = reactive({name:"obj_name", tag:"obj_tag"})
 const value = "Hello Value!"
 const tvalue = true
 const fvalue = false
@@ -17,23 +19,84 @@ const objectOfAttrs = {
 function doSomthing() {
   alert("do somthing")
 }
+
+function ref_obj_reset() {
+  ref_obj.value = {name:"ref_obj_name", tag:"ref_obj_tag"}
+}
+
+let refmsg_upper_ro = computed(() => {
+  return ref_msg.value.toUpperCase()
+})
+
+let refmsg_upper_rw = computed({
+  set(new_val) {
+    ref_msg.value = new_val.toLowerCase()
+  },
+
+  get() {
+      return ref_msg.value.toUpperCase()
+  }
+})
+
+// 监视ref
+let watch_result = ref()
+let stop_watch = watch(ref_msg, (new_val, old_val) => { // watch双参数即new, old
+  watch_result.value = old_val + " => " + new_val
+  if (new_val.length > 20) {  // 选择停止监视的时机
+    stop_watch()
+  }
+}, {immediate:true})
+
+// 监视ref对象（reactive对象）
+// 说明：当watch object的时候，会发现，new value和old value是一样的，因为总是来自同一引用
+// 用reactive与用ref类似，并且会隐式的标注deep:true
+watch(ref_obj, (val) => { // watch单参即new value
+  watch_result.value = val
+}, {deep:true})
+
+// 监视getter函数
+// 注意，watch会按照代码顺序，依次执行，后面覆盖前面
+watch(() => ref_obj.value.name, (val) => { // 只watch一个对象中的具体元素
+  watch_result.value = val
+})
+
+// 监听多个内容（数组）
+watch ([ref_msg, ref_obj, () => ref_obj.value.name], (val) => {
+  console.log(val)
+})
 </script>
 
 <template>
-  <!-- 变量的单项绑定 -->
-  <h3> 变量的单项绑定 </h3>
+  <!-- 变量的基本用法 -->
+  <h3> 变量的基本用法 </h3>
   <p>{{ msg }}</p>                        <!-- 变量作为内容的使用：双打括号 -->
   <p>{{ tvalue ? "True" : "False" }}</p>  <!-- 双括号里支持完整的JS语法 -->
-  <input v-bind:value=msg />    <!-- 单向数据绑定：v-bind:{name}=var -->
-  <input :value="msg" />        <!-- 单向数据绑定（简写）：:{name}=var -->
-  <input :value />              <!-- 单向数据绑定（简写：属性与变量同名）：:{name} -->
-  <input :[attrName]="value" /> <!-- 动态绑定属性名：:[var]=var -->
-  <div v-bind="objectOfAttrs"></div> <!-- 多值绑定 -->
+  <input v-bind:value=msg /> <br/>    <!-- v-bind用法（标准）：v-bind:{name}=var -->
+  <input :value="msg" /> <br/>        <!-- v-bind用法（简写）：:{name}=var -->
+  <input :value /> <br/>              <!-- v-bind用法（同名）：属性与变量同名:{name} -->
+  <input :[attrName]="value" /> <br/> <!-- v-bind用法（属性）：动态绑定属性名：:[var]=var -->
+  <div v-bind="objectOfAttrs"></div>  <!-- v-bind用法（多值）：多值绑定 -->
 
-  <!-- 变量的双项绑定 -->
+  <!-- 变量的单/双项绑定 -->
   <h3> 变量的双项绑定 </h3>
-  <input :value="refmsg" />   <!-- 单向数据绑定：使用响应式变量 -->
-  <input v-model="refmsg" />  <!-- 双向向数据绑定：v-model默认绑定value属性 -->
+  <span>单向绑定：</span> <input :value="ref_msg" /> <br/>  <!-- 响应式变量 -->
+  <span>对象绑定：</span> <input :value="reactive_obj" /> <br/>  <!-- 响应式变量（reactive） -->
+  <span>双向绑定：</span> <input v-model="ref_msg" />       <!-- v-model默认绑定value属性 -->
+
+  <!-- computed计算属性用法 -->
+  <h3> computed计算属性用法 </h3>
+  <input v-model="ref_msg" />
+  <p>{{ refmsg_upper_ro }}</p>        <!-- 只读计算属性 -->
+  <input v-model="refmsg_upper_rw" /> <!-- 读写计算属性 -->
+
+  <!-- watch监视用法 -->
+  <h3> watch监视用法 </h3>
+  <span>watch ref: </span> <input v-model="ref_msg" /> <br/>
+  <span>watch ref(obj): </span>
+  <input v-model="ref_obj.name" />
+  <input v-model="ref_obj.tag">
+  <button @click="ref_obj_reset">重置</button>  <br/>
+  <p>watch结果: {{ watch_result }}</p>
 
   <!-- v-if 用法 -->
   <h3> v-if 用法 </h3>
